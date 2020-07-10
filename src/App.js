530f694aa19e6faf4ebaf6d69505e878d5bb0bb8
sudhir.kunnure <sudhir.kunnure@capgemini.com>
2020-07-10 10:00:49 +0530

    removed declarations

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import  Home  from './components/Home';
-import { FetchData } from './components/FetchData';
 import { AddRecord } from './components/AddRecord';
 import { UpdateRecord } from './components/updateRecord';
-import { Search } from './components/Search';
 import { Authorize } from './components/Authorize';
 import { Audit } from './components/Audit';
 export default class App extends Component {
@@ -20,9 +18,7 @@ export default class App extends Component {
          <Route exact path='/home' component={Home} />
         <Route path='/addrecord' component={AddRecord} />
           <Route path='/updateRecord' component={UpdateRecord} />
-        <Route path='/fetchdata' component={FetchData} />
         <Route path='/authorize' component={Authorize} />
-        <Route path='/search' component={Search} />
         <Route path='/audit' component={Audit} />
        
       </Layout>
